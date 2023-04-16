44d3dfb6de08aac316ee2615653a56957ee8c742
stefifm <bruerastefania@gmail.com>
2023-04-16 11:59:16 -0300

    Add global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Container from './components/Container/Container'
 import Header from './components/Header/Header'
+import GlobalStyles from './GlobalStyles'
 
 function App() {
   return (
     <>
+      <GlobalStyles />
       <Header />
       <Container />
     </>
