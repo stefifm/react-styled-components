5393e6b2b41dc117346eb290e688fde23b0e9b87
stefifm <bruerastefania@gmail.com>
2023-04-16 20:40:24 -0300

    Add media queries to styled components

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,14 +1,30 @@
+import styled from 'styled-components'
 import Account from '../Account/Account'
 import Title from '../Title/Title'
 
+const StyledContainer = styled.div`
+  background-color: #f1f1f1;
+  min-height: 90vh;
+  padding: 0px 15vw;
+`
+
+const StyledContent = styled.section`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  @media (max-width: 800px) {
+    flex-direction: column;
+  }
+`
+
 function Container() {
   return (
-    <div className='container'>
+    <StyledContainer>
       <Title>Smart Bank</Title>
-      <section className='content'>
+      <StyledContent>
         <Account />
-      </section>
-    </div>
+      </StyledContent>
+    </StyledContainer>
   )
 }
 
